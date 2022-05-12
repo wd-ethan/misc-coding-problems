7382fb743b218efda78877bc1e42fa27f988ce17
Ethan Wright <78000710+wd-ethan@users.noreply.github.com>
2022-05-11 18:09:01 -0700

    Create substringNoRepeat.java

---
+++ substringNoRepeat.java
@@ -0,0 +1,37 @@
+import java.util.Hashtable;
+
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+    
+        Hashtable<Character, Integer> hash = new Hashtable<Character, Integer>();
+        int i;
+        // first character in no-repeat string
+        int j = 0;
+        int saved = 1;
+        
+        if (s.length() == 0){
+            return 0;
+        }
+        
+        for (i = 0; i < s.length(); i++) {
+            
+            //this is essentially a fail, we have seen this character before
+            if (hash.get(s.charAt(i)) != null && hash.get(s.charAt(i)) >= j){
+                //save our count
+                if ((i-j) > saved){
+                    saved  = (i-j);
+                }
+                j = hash.get(s.charAt(i))+1;
+                hash.put(s.charAt(i), i);
+                
+            // else we continue on
+            } else{
+                hash.put(s.charAt(i), i);
+            }
+        }
+        if ((i-j) > saved){
+            saved  = (i-j);
+        }
+        return saved;
+    }
+}
