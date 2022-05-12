36689bd5cc3eb26c1d76e7796f36127436c67040
Ethan Wright <78000710+wd-ethan@users.noreply.github.com>
2022-05-11 18:05:40 -0700

    Create validParenthesis.java

---
+++ validParenthesis.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Solution {
+    public boolean isValid(String s) {
+        
+        Stack stack = new Stack();
+        Map<Character, Character> staticMap = new HashMap<>();
+        
+        staticMap.put('(', ')');
+        staticMap.put('{', '}');
+        staticMap.put('[', ']');
+        
+        
+        for (int i = 0; i < s.length(); i++) {
+            
+            if (s.charAt(i) == '(' || s.charAt(i) == '[' || s.charAt(i) == '{'){
+                stack.push(staticMap.get(s.charAt(i)));
+            } else if (s.charAt(i) == ')' || s.charAt(i) == ']' || s.charAt(i) == '}'){
+                if (stack.isEmpty()) {
+                    return false;
+                } else if (!stack.pop().equals(s.charAt(i))) {
+                    return false;
+                }
+            }
+        }
+        
+        if (stack.isEmpty()){
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
