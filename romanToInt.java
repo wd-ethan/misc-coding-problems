19de57b3c1718ffbbc6589adaf927308d7499736
Ethan Wright <78000710+wd-ethan@users.noreply.github.com>
2022-05-11 18:09:50 -0700

    Create romanToInt.java

---
+++ romanToInt.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int romanToInt(String s) {
+        char[] charArr = s.toCharArray();
+        int i;
+        int sum = 0;
+        // check each element in char array
+        for (i = 0; i < s.length()-1; i++){
+            // this is our "weird" case
+            if (getValue(charArr[i]) < getValue(charArr[i+1])) {
+                sum += (getValue(charArr[i+1]) - getValue(charArr[i]));
+                i++;
+            } else {
+                sum += getValue(charArr[i]);
+            }
+        }
+        // off by one probelm..
+        if (i < s.length()) {
+            sum += getValue(charArr[i]);
+        }
+        
+        return sum;
+    }
+    
+    // easy switch case, cleans code above
+    public int getValue(char c) {
+        switch(c) {
+            case 'I':
+                return 1;
+            case 'V':
+                return 5;
+            case 'X':
+                return 10;
+            case 'L':
+                return 50;
+            case 'C':
+                return 100;
+            case 'D':
+                return 500;
+            case 'M':
+                return 1000;
+            default:
+                return 0;
+        } 
+    }
+}
