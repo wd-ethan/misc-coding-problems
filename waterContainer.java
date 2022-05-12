9b8e31291a3de19c2b5d9f9aa1fcadd9b8b313f6
Ethan Wright <78000710+wd-ethan@users.noreply.github.com>
2022-05-11 18:07:31 -0700

    Create waterContainer.java

---
+++ waterContainer.java
@@ -0,0 +1,46 @@
+class Solution {
+    
+    public int maxArea(int[] height) {
+        
+        //start with widest container
+        
+        // pointers
+        int i = 0, j = height.length-1;
+        // keep track of max
+        int maxArea = calcArea(height, i, j);
+        
+        
+        while (i < j) {
+            
+            // the idea here is to move the limiting one...
+            if (i == minHeight(height, i , j)) {
+                 i++;
+            } else {
+                j--;
+            }
+            
+            // now check if that optimized
+            if (maxArea < calcArea(height, i, j)) {
+                maxArea = calcArea(height, i, j);
+            }
+        }
+        
+        return maxArea;
+        
+    }
+    
+    
+    // return min height for purpose of calculating area
+    public int minHeight(int[] height, int i, int j) {
+        if (height[i] > height[j]) {
+            return j;
+        }
+        return i;
+    }
+    
+    // simple calculation of area for this specfic problem
+    public int calcArea(int[] height, int i, int j) {
+        return (j-i)*(Math.min(height[i], height[j]));
+    }
+    
+}
